c577014b7179b7fe983e6895657b4ef9d3516765
karodes <sameer.karode@cdk.com>
2018-08-30 22:16:09 +0530

    adding problem statement to phone-kata

--- phone-number-kata/src/main/java/com/mycompany/phonenumber/PhoneNumber.java
+++ phone-number-kata/src/main/java/com/mycompany/phonenumber/PhoneNumber.java
@@ -1,5 +1,9 @@
 package com.mycompany.phonenumber;
 
+/* problem statement
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+*/
+
 public class PhoneNumber
 {
     public static String createNumber(int[] arr) {
