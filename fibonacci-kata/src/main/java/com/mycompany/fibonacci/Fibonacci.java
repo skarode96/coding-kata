265e390cb0d0a0b7d92811632664f4bae95cd7bb
karodes <sameer.karode@cdk.com>
2018-08-31 19:29:16 +0530

    adding fibonacci kata

---
+++ fibonacci-kata/src/main/java/com/mycompany/fibonacci/Fibonacci.java
@@ -0,0 +1,23 @@
+package com.mycompany.fibonacci;
+
+public class Fibonacci
+{
+
+    public static int getNthTerm(int i) {
+        validateArgument(i);
+        return isArgumentBetweenZeroAndTwo(i) ? i-1 :  getNthTerm(i-1) + getNthTerm( i-2 );
+    }
+
+    private static void validateArgument(int i) {
+        if(isArgumentInvalid(i))
+           throw new IllegalArgumentException();
+    }
+
+    private static boolean isArgumentInvalid(int i) {
+        return i<= 0 ? true : false;
+    }
+
+    private static boolean isArgumentBetweenZeroAndTwo(int i) {
+        return i<=2 && i>0 ? true : false;
+    }
+}
---
+++ fibonacci-kata/src/test/java/com/mycompany/fibonacci/FibonacciTest.java
@@ -0,0 +1,30 @@
+package com.mycompany.fibonacci;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FibonacciTest
+{
+    @Test
+    public void testGetFirstTermOfFibonacci(){
+        Assert.assertEquals(0,Fibonacci.getNthTerm(1));
+    }
+    @Test
+    public void testGetSecondTermOfFibonacci(){
+        Assert.assertEquals(1,Fibonacci.getNthTerm(2));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowInvalidArgExceptionIfNumberIsLessThanZero() {
+        Fibonacci.getNthTerm(-1);
+    }
+
+    @Test
+    public void shouldReturnNthTermOfFibonacci() {
+        Assert.assertEquals(2, Fibonacci.getNthTerm(4));
+        Assert.assertEquals(3, Fibonacci.getNthTerm(5));
+        Assert.assertEquals(5, Fibonacci.getNthTerm(6));
+        Assert.assertEquals(8, Fibonacci.getNthTerm(7));
+    }
+}
