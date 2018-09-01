7ee401731faa9718ea52f8e6ce2aae4251f14cf6
karodes <sameer.karode@cdk.com>
2018-09-01 11:53:52 +0530

    Formatting content

--- tower-of-hanoi/src/test/java/com/mycompany/towerofhanoi/TowerOfHanoiTest.java
+++ tower-of-hanoi/src/test/java/com/mycompany/towerofhanoi/TowerOfHanoiTest.java
@@ -8,8 +8,7 @@ import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-public class TowerOfHanoiTest
-{
+public class TowerOfHanoiTest {
     private PrintStream printStream;
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
@@ -21,8 +20,8 @@ public class TowerOfHanoiTest
 
     @Test
     public void shouldPlaceFromSourceToDestinationIfOnePresent() {
-        TowerOfHanoi.solveTower(1,"A","B","C");
-        Assert.assertEquals("Move Disk 1 from A to B\n",outContent.toString());
+        TowerOfHanoi.solveTower(1, "A", "B", "C");
+        Assert.assertEquals("Move Disk 1 from A to B\n", outContent.toString());
     }
 
     @Test
@@ -34,7 +33,13 @@ public class TowerOfHanoiTest
     @Test
     public void shouldPlaceForHeightThree() {
         TowerOfHanoi.solveTower(3, "A", "B", "C");
-        Assert.assertEquals("Move Disk 1 from A to B\nMove Disk 2 from A to C\nMove Disk 1 from B to C \nMove Disk 3 from A to B \nMove Disk 1 from C to A\nMove Disk 2 from C to B\nMove Disk 1 from A to B\n", outContent.toString());
+        Assert.assertEquals("Move Disk 1 from A to B\n" +
+                "Move Disk 2 from A to C\n" +
+                "Move Disk 1 from B to C\n" +
+                "Move Disk 3 from A to B\n" +
+                "Move Disk 1 from C to A\n" +
+                "Move Disk 2 from C to B\n" +
+                "Move Disk 1 from A to B\n", outContent.toString());
     }
 
     @After
